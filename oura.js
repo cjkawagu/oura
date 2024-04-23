38e0bdf8155ec32832f7f4727ad7464d705fc8d2
josephineousc <onggowar@usc.edu>
2024-04-23 15:32:53 -0700

    screenshot and save

--- oura.js
+++ oura.js
@@ -1,183 +1,184 @@
 function toggleTheme() {
-  document.body.classList.toggle("dark-mode"); // Toggle between dark and light mode
-  const isDarkMode = document.body.classList.contains("dark-mode"); // Check if dark mode is active
-  localStorage.setItem("theme", isDarkMode ? "dark" : "light"); // Store the preference in localStorage
-}
-
-function highlightTab(tab) {
-  const tabs = document.querySelectorAll(".tab");
-  tabs.forEach((tab) => {
-    tab.classList.remove("selected");
-  });
-  tab.classList.add("selected");
-}
-
-function showPage(page) {
-  const mainContent = document.querySelector(".main-content");
-  mainContent.innerHTML = ""; // Clear previous content
-
-  let contentTitle = "";
-  let contentHTML = "";
-
-  if (page === "weight-dose") {
-    contentTitle = "Sleep Quality";
-    contentHTML = `
-          <h1>&nbsp; ${contentTitle}</h1>
-          <div class="graph-container">
-            <br>
-            <div class="tab-container">
-              <button class="tab" onclick="highlightTab(this)">Daily</button>
-              <button class="tab" onclick="highlightTab(this)">Weekly</button>
-              <button class="tab" onclick="highlightTab(this)">Monthly</button>
-              <button class="tab" onclick="highlightTab(this)">Yearly</button>
-            </div>
-            <div class="content-area" id="graph">
-                <iframe src="graph/ouragraph.html" class="ouraGraph1"></iframe> 
-            </div>
+    document.body.classList.toggle("dark-mode"); // Toggle between dark and light mode
+    const isDarkMode = document.body.classList.contains("dark-mode"); // Check if dark mode is active
+    localStorage.setItem("theme", isDarkMode ? "dark" : "light"); // Store the preference in localStorage
+  }
+  
+  function highlightTab(tab) {
+    const tabs = document.querySelectorAll(".tab");
+    tabs.forEach((tab) => {
+      tab.classList.remove("selected");
+    });
+    tab.classList.add("selected");
+  }
+  
+  function showPage(page) {
+    const mainContent = document.querySelector(".main-content");
+    mainContent.innerHTML = ""; // Clear previous content
+  
+    let contentTitle = "";
+    let contentHTML = "";
+  
+    if (page === "weight-dose") {
+      contentTitle = "Sleep Quality";
+      contentHTML = `
+        <h1>&nbsp; ${contentTitle}</h1>
+        <div class="graph-container">
+          <br>
+          <div class="tab-container">
+            <button class="tab" onclick="highlightTab(this)">Daily</button>
+            <button class="tab" onclick="highlightTab(this)">Weekly</button>
+            <button class="tab" onclick="highlightTab(this)">Monthly</button>
+            <button class="tab" onclick="highlightTab(this)">Yearly</button>
           </div>
-        `; // OKAY BASICALLY JUST CHANGE THE URL OF THE GRAPH AND IT SHOULD BE ABLE TO POPULATE IT! THIS ONE IS OURAGRAPH1
-  } else if (page === "sleep-calories") {
-    contentTitle = "Weight & Activity";
-    contentHTML = `
-            <h1>&nbsp; ${contentTitle}</h1>
-            <div class="graph-container">
-                <br>
-                <div class="tab-container">
-                    <button class="tab" onclick="highlightTab(this)">Daily</button>
-                    <button class="tab" onclick="highlightTab(this)">Weekly</button>
-                    <button class="tab" onclick="highlightTab(this)">Monthly</button>
-                    <button class="tab" onclick="highlightTab(this)">Yearly</button>
-                </div>
-                <div class="content-area" id="graph">
-                    <iframe src="graph2/graph2.html" class="ouraGraph2"></iframe>
-                </div>
-            </div>
-        `; // OKAY BASICALLY JUST CHANGE THE URL OF THE GRAPH AND IT SHOULD BE ABLE TO POPULATE IT! THIS ONE IS OURAGRAPH2
-  } else if (page === "patient-overview") {
-    contentTitle = "Patient Overview";
-    contentHTML = `
+          <div class="content-area" id="graph">
+            <iframe src="graph/ouragraph.html" class="ouraGraph1"></iframe> 
+          </div>
+        </div>
+      `;
+    } else if (page === "sleep-calories") {
+      contentTitle = "Weight & Activity";
+      contentHTML = `
+        <h1>&nbsp; ${contentTitle}</h1>
+        <div class="graph-container">
+          <br>
+          <div class="tab-container">
+            <button class="tab" onclick="highlightTab(this)">Daily</button>
+            <button class="tab" onclick="highlightTab(this)">Weekly</button>
+            <button class="tab" onclick="highlightTab(this)">Monthly</button>
+            <button class="tab" onclick="highlightTab(this)">Yearly</button>
+          </div>
+          <div class="content-area" id="graph">
+            <iframe src="graph2/graph2.html" class="ouraGraph2"></iframe>
+          </div>
+        </div>
+      `;
+    } else if (page === "patient-overview") {
+      contentTitle = "Patient Overview";
+      contentHTML = `
         <h1>&nbsp; ${contentTitle}</h1>
         
         <div class="graph-container patient-overview">
-                <div class="patient-info">
-                <div class="info-group">
-                <div class="info-item">
-                    <label for="patientName">Patient Name:</label>
-                    <input type="text" id="patientName" placeholder="Enter patient name">
-                </div>
-                <div class="info-item">
-                    <label for="patientID">Patient ID:</label>
-                    <input type="text" id="patientID" placeholder="Enter patient ID">
-                </div>
-                </div>
-                <div class="info-group">
-                <div class="info-item">
-                    <label for="dateOfBirth">Date of Birth:</label>
-                    <input type="date" id="dateOfBirth">
-                </div>
-                <div class="info-item">
-                    <label for="gender">Gender:</label>
-                    <input type="text" id="gender" placeholder="Enter gender">
-                </div>
-                </div>
-                <div class="info-group">
-                <div class="info-item">
-                    <label for="allergies">Allergies:</label>
-                    <input type="text" id="allergies" placeholder="Enter allergies">
-                </div>
-                <div class="info-item">
-                    <label for="currentMedication">Current Medication:</label>
-                    <input type="text" id="currentMedication" placeholder="Enter medication">
-                </div>
-                </div>
-            </div>
-            
-            <div class="patient-details">
-                <div class="details-left">
-                <h3>Provider Notes</h3>
-                <textarea id="providerNotes" placeholder="Enter provider notes"></textarea>
-                </div>
-            
-                <div class="details-right">
-                <br> <br> <br>
-            
-                <div class="detail-item">
-                    <h3>Vitals</h3>
-                    <textarea id="vitals" placeholder="Enter vitals"></textarea>
-                </div>
-            
-                <div class="detail-item">
-                    <h3>Prescriptions</h3>
-                    <textarea id="prescriptions" placeholder="Enter prescriptions"></textarea>
-                </div>
-            
-                <div class="detail-item">
-                    <h3>Lab Results</h3>
-                    <textarea id="labResults" placeholder="Enter lab results"></textarea>
-                </div>
-            
-                <div class="detail-item">
-                    <h3>Medical Images</h3>
-                    <textarea id="medicalImages" placeholder="Enter medical images"></textarea>
-                </div>
-            
-                <div class="detail-item">
-                    <h3>Medical History</h3>
-                    <textarea id="medicalHistory" placeholder="Enter medical history"></textarea>
-                </div>
-            
-                <div class="detail-item">
-                    <h3>Documents</h3>
-                    <textarea id="documents" placeholder="Enter documents"></textarea>
-                </div>
-                </div>
-            </div>
+        <div class="patient-info">
+        <div class="info-group">
+          <div class="info-item">
+            <label for="patientName">Patient Name:</label>
+            <input type="text" id="patientName" placeholder="Enter patient name">
+          </div>
+          <div class="info-item">
+            <label for="patientID">Patient ID:</label>
+            <input type="text" id="patientID" placeholder="Enter patient ID">
+          </div>
+        </div>
+        <div class="info-group">
+          <div class="info-item">
+            <label for="dateOfBirth">Date of Birth:</label>
+            <input type="date" id="dateOfBirth">
+          </div>
+          <div class="info-item">
+            <label for="gender">Gender:</label>
+            <input type="text" id="gender" placeholder="Enter gender">
+          </div>
+        </div>
+        <div class="info-group">
+          <div class="info-item">
+            <label for="allergies">Allergies:</label>
+            <input type="text" id="allergies" placeholder="Enter allergies">
+          </div>
+          <div class="info-item">
+            <label for="currentMedication">Current Medication:</label>
+            <input type="text" id="currentMedication" placeholder="Enter medication">
+          </div>
+        </div>
+      </div>
+      
+      <div class="patient-details">
+        <div class="details-left">
+          <h3>Provider Notes</h3>
+          <textarea id="providerNotes" placeholder="Enter provider notes"></textarea>
+        </div>
+      
+        <div class="details-right">
+          <br> <br> <br>
+      
+          <div class="detail-item">
+            <h3>Vitals</h3>
+            <textarea id="vitals" placeholder="Enter vitals"></textarea>
+          </div>
+      
+          <div class="detail-item">
+            <h3>Prescriptions</h3>
+            <textarea id="prescriptions" placeholder="Enter prescriptions"></textarea>
+          </div>
+      
+          <div class="detail-item">
+            <h3>Lab Results</h3>
+            <textarea id="labResults" placeholder="Enter lab results"></textarea>
+          </div>
+      
+          <div class="detail-item">
+            <h3>Medical Images</h3>
+            <textarea id="medicalImages" placeholder="Enter medical images"></textarea>
+          </div>
+      
+          <div class="detail-item">
+            <h3>Medical History</h3>
+            <textarea id="medicalHistory" placeholder="Enter medical history"></textarea>
+          </div>
+      
+          <div class="detail-item">
+            <h3>Documents</h3>
+            <textarea id="documents" placeholder="Enter documents"></textarea>
+          </div>
+          
+        </div>
 
-            <div class="save-button-container">
+        
+      </div>
+
+      <div class="save-button-container">
             <div class="save-button-wrapper">
               <button id="saveButton" onclick="savePatientInfo()">Save</button>
             </div>
           </div>
-         </div>
-
-        </div>
+          </div>
+          
   
-        
+          
+        </div>
       `;
+    }
+  
+    const contentDiv = document.createElement("div");
+    contentDiv.innerHTML = contentHTML;
+    mainContent.appendChild(contentDiv);
+  
+    const weeklyTab = contentDiv.querySelector(".tab:nth-child(2)");
+    if (weeklyTab) {
+      highlightTab(weeklyTab);
+    }
+  
+    document.querySelectorAll(".sidebar-item").forEach((item) => {
+      item.classList.remove("selected"); // Unselect all sidebar items
+    });
+    document.querySelector(`[data-page-link="${page}"]`).classList.add("selected"); // Highlight selected item
+  
+    localStorage.setItem("lastPage", page); // Persist the selected page
   }
-
-  const contentDiv = document.createElement("div");
-  contentDiv.innerHTML = contentHTML;
-  mainContent.appendChild(contentDiv);
-
-  const weeklyTab = contentDiv.querySelector(".tab:nth-child(2)");
-  if (weeklyTab) {
-    highlightTab(weeklyTab);
+  
+  function saveProviderName() {
+    const providerName = document.getElementById("providerName").value;
+    localStorage.setItem("providerName", providerName);
   }
-
-  document.querySelectorAll(".sidebar-item").forEach((item) => {
-    item.classList.remove("selected"); // Unselect all sidebar items
-  });
-  document
-    .querySelector(`[data-page-link="${page}"]`)
-    .classList.add("selected"); // Highlight ta selected item
-
-  localStorage.setItem("lastPage", page); // Persist the selected page
-}
-
-function saveProviderName() {
-  const providerName = document.getElementById("providerName").value;
-  localStorage.setItem("providerName", providerName);
-}
-
-function loadProviderName() {
-  const providerName = localStorage.getItem("providerName");
-  if (providerName) {
-    document.getElementById("providerName").value = providerName;
+  
+  function loadProviderName() {
+    const providerName = localStorage.getItem("providerName");
+    if (providerName) {
+      document.getElementById("providerName").value = providerName;
+    }
   }
-}
-
-function savePatientInfo() {
+  
+  function savePatientInfo() {
     const patientName = document.getElementById("patientName").value;
     const patientID = document.getElementById("patientID").value;
     const dateOfBirth = document.getElementById("dateOfBirth").value;
@@ -209,9 +210,25 @@ function savePatientInfo() {
     };
   
     localStorage.setItem("patientInfo", JSON.stringify(patientInfo));
-}
-
-function loadPatientInfo() {
+  
+    // Display a confirmation message
+    alert("Patient information saved successfully!");
+  
+    // Capture a screenshot of the page
+    captureScreenshot();
+  }
+  
+  function captureScreenshot() {
+    html2canvas(document.body).then(function (canvas) {
+      var screenshotURL = canvas.toDataURL("image/png");
+      var link = document.createElement("a");
+      link.href = screenshotURL;
+      link.download = "patient_overview_screenshot.png";
+      link.click();
+    });
+  }
+  
+  function loadPatientInfo() {
     const patientInfo = JSON.parse(localStorage.getItem("patientInfo"));
     if (patientInfo) {
       document.getElementById("patientName").value = patientInfo.patientName;
@@ -228,10 +245,10 @@ function loadPatientInfo() {
       document.getElementById("medicalHistory").value = patientInfo.medicalHistory;
       document.getElementById("documents").value = patientInfo.documents;
     }
-}
-
-// Restore sidebar selection, theme mode, provider name, and patient info on page load
-window.addEventListener("load", () => {
+  }
+  
+  // Restore sidebar selection, theme mode, provider name, and patient info on page load
+  window.addEventListener("load", () => {
     const lastPage = localStorage.getItem("lastPage");
     const savedTheme = localStorage.getItem("theme");
   
